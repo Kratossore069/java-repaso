286119424e16ad3e1ef92596ab61e7e4185a6049
kratossore069 <victorgabrielarostegui@gmail.com>
2021-03-18 13:03:22 +0000

    List_de_objetos

--- dswJava/src/Run/Main3.java
+++ dswJava/src/Run/Main3.java
@@ -17,13 +17,37 @@ public class Main3 {
             equipos.add(pf);
         }
 
-        Iterator<PartidoFutbol> it=equipos.iterator();
-        while(it.hasNext()){
-            PartidoFutbol partido=it.next();
-            System.out.println(partido.getEquipoLocal()+" "+
-            partido.getEquipoVisitante()+" "+
-            partido.getGolesLocal()+" "+
-            partido.getGolesVisitante());
+        Iterator<PartidoFutbol> it = equipos.iterator();
+        while (it.hasNext()) {
+            PartidoFutbol partido = it.next();
+            System.out.println(partido.getEquipoLocal() + " " + partido.getEquipoVisitante() + " "
+                    + partido.getGolesLocal() + " " + partido.getGolesVisitante());
         }
+
+        // creamos una List que contendra una List de String
+        ArrayList<ArrayList<String>> listaDeLista = new ArrayList<>();
+
+        // creamos la primera List de String y le añadimos elementos
+        ArrayList<String> listaStringUno = new ArrayList<>();
+        listaStringUno.add("Cadena uno");
+        listaStringUno.add("Cadena dos");
+        // añadimos la List de String a la List de List
+        listaDeLista.add(listaStringUno);
+
+        // creamos la segunda List de String y le añadimos elementos
+        ArrayList<String> listaStringDos = new ArrayList<>();
+        listaStringDos.add("Cadena tres");
+        // añadimos la List de String a la List de List
+        listaDeLista.add(listaStringDos);
+
+        // creamos la tercera List de String y le añadimos elementos
+        ArrayList<String> listaStringTres = new ArrayList<>();
+        listaStringTres.add("Cadena cuatro");
+        listaStringTres.add("Cadena cinco");
+        // añadimos la List de String a la List de List
+        listaDeLista.add(listaStringTres);
+
+        // imprimimos la lista de listas
+        System.out.println(listaDeLista);
     }
 }
