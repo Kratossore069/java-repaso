a6426334e7dc7ccd276c2b7099b7b5ffeaad522a
kratossore069 <victorgabrielarostegui@gmail.com>
2021-04-16 17:12:49 +0100

    Adelanto con los ficheros y las excepciones

--- misExcepciones/excepciones/src/main/java/es/iesptocruz/victor/Fichero.java
+++ misExcepciones/excepciones/src/main/java/es/iesptocruz/victor/Fichero.java
@@ -1,7 +1,9 @@
 package es.iesptocruz.victor;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 import excepciones.FicheroException;
 
@@ -30,41 +32,109 @@ public class Fichero {
      */
     public void crearFichero(String nombre) throws FicheroException {
         File file;
-        if (!existe(nombre)) { //Lío de llaves
-            file = new File(nombre);
-        try {
-                file.createNewFile();
-            
-        } catch (IOException exception) {
-            throw new FicheroException("Error al crear el fichero",exception);
+
+        if (existe(nombre)) {
+            throw new FicheroException("No se puede crear un fichero que ya existe");
         }
+        file = new File(nombre);
+        try {
+            file.createNewFile();
+        } catch (IOException exception) {
+            throw new FicheroException("Se ha producido un error creando el fichero", exception);
+        }
+    }
 
     /**
-         * Metodo encargado de borrar fichero
-         * @param nombre del fichero
-         * @throws FicheroException error controlado
-         */
-    public void borrarFichero(String nombre) throws FicheroException {
-            File file;
-            if (!existe(nombre)) { //Lío de llaves
-                file = new File(nombre);
-            try {
-                    file.createNewFile();
-                
-            } catch (IOException exception) {
-                throw new FicheroException("Error al crear el fichero",exception);
-            }
+     * Funcion que muestra si se puede leer o no un documento
+     * 
+     * @param nombre del documento
+     * @return true or false
+     * @throws FicheroException error controlado
+     */
+    public boolean sePuedeLeer(String nombre) throws FicheroException {
+        File file;
+        boolean legible = false;
 
-            // public void crearFichero2(String nombre) throws FicheroException {
-            //     File file;
-          
-            //     if (existe(nombre)) {
-            //        throw new FicheroException("No se puede crear un fichero que ya existe");
-            //     }
-            //     file = new File(nombre);
-            //     try {
-            //        file.createNewFile();
-            //     } catch (IOException exception) {
-            //        throw new FicheroException("Se ha producido un error creando el fichero", exception);
-            //     }
-            //  }
+        if (existe(nombre)) {
+            file = new File(nombre);
+            legible = file.canRead();
+        } else {
+            throw new FicheroException("El fichero que intenta leer no existe");
+        }
+        return legible;
+    }
+
+    /**
+     * Funcion para leer el documento
+     * 
+     * @param nombre del documento
+     * @return
+     * @throws FicheroException
+     */
+    public String mostrarDocumento(String nombre) throws FicheroException {
+        File file;
+        String datos = "";
+
+        if (existe(nombre) && sePuedeLeer(nombre)) {
+            try {
+                file = new File(nombre);
+                Scanner myReader = new Scanner(file);
+                while (myReader.hasNextLine()) {
+                    datos = myReader.nextLine();
+                }
+                myReader.close();
+            } catch (IOException exception) {
+                throw new FicheroException("Error al leer el fichero: ", exception);
+            }
+        } else {
+            throw new FicheroException("El fichero que intenta leer no existe");
+        }
+        return datos;
+    }
+
+    /**
+     * Funcion que permite saber si el fichero es redactable
+     * 
+     * @param nombre del fichero
+     * @return true or false
+     * @throws FicheroException error controlado
+     */
+    public boolean sePuedeRedactar(String nombre) throws FicheroException {
+        File file;
+        boolean redactable = false;
+
+        if (existe(nombre)) {
+            file = new File(nombre);
+            redactable = file.canWrite();
+        } else {
+            throw new FicheroException("El fichero que intenta escribir no existe");
+        }
+        return redactable;
+    }
+
+    /**
+     * Funcion que escribe en un fichero
+     * @param nombre del fichero
+     * @return mensaje de haber escrito
+     * @throws FicheroException error controlado
+     */
+    public String redactarDocumento(String nombre) throws FicheroException {
+        FileWriter fw;
+        String datos = "Hola mundo";
+        String resultado = "";
+
+        if (existe(nombre) && sePuedeRedactar(nombre)) {
+            try {
+                fw = new FileWriter(nombre);
+                fw.write(datos);
+                fw.close();
+                resultado = "Escrito con éxito";
+            } catch (IOException exception) {
+                throw new FicheroException("Error al escribir", exception);
+            }
+        } else {
+            throw new FicheroException("El fichero que intenta escribir no existe");
+        }
+        return resultado;
+    }
+}
