bc9298682ef38f03c9ffe1b650650c47ab727f14
kratossore069 <victorgabrielarostegui@gmail.com>
2021-04-22 17:27:51 +0100

    Ultimos test y funciones de ficheros

--- mvcUsuarios/mvnUsuarios/src/main/java/es/iesptocruz/victor/modelo/UsuarioModelo.java
+++ mvcUsuarios/mvnUsuarios/src/main/java/es/iesptocruz/victor/modelo/UsuarioModelo.java
@@ -1,49 +1,68 @@
 package es.iesptocruz.victor.modelo;
 
+import java.io.File;
+
 import es.iesptocruz.victor.exceptions.FicheroException;
 
 public class UsuarioModelo {
-    Fichero fichero;
+   Fichero fichero;
 
-    public UsuarioModelo() {
-        fichero = new Fichero();
-    }
+   public UsuarioModelo() {
+      fichero = new Fichero();
+   }
 
-    /**
-     * Metodo que crea un archivo o lo reemplaza y escribe sobre el mismo
-     * @param ruta del archivo con su nombre
-     * @param texto a insertar en el archivo
-     * @throws FicheroException error controlado
-     */
-    public void insertar(String ruta, String texto) throws FicheroException {
-        fichero.crear(ruta, texto);
-     }
-  
-     /**
-      * Metodo para eliminar un archivo
-      * @param nombreFichero a eliminar
-      * @throws FicheroException error controlado
-      */
-     public void eliminar(String nombreFichero) throws FicheroException {
-        fichero.eliminar(nombreFichero);
-     }
-  
-     public Fichero buscar(String identificador) {
-       fichero = null;
-  
-       return fichero;
-     }
-  
-     public void modificar(Fichero fichero) {
-        //lo que sea
-     }
+   /**
+    * Metodo que crea un archivo o lo reemplaza y escribe sobre el mismo
+    * 
+    * @param ruta  del archivo con su nombre
+    * @param texto a insertar en el archivo
+    * @throws FicheroException error controlado
+    */
+   public void insertar(String ruta, String texto) throws FicheroException {
+      fichero.crear(ruta, texto);
+   }
 
-     /**
-      * Funcion que lee un fichero
-      * @param rutaFichero del que leer algo
-      * @throws FicheroException error controlado
-      */
-     public String leer(String rutaFichero) throws FicheroException{
+   /**
+    * Metodo para eliminar un archivo
+    * 
+    * @param nombreFichero a eliminar
+    * @throws FicheroException error controlado
+    */
+   public void eliminar(String nombreFichero) throws FicheroException {
+      fichero.eliminar(nombreFichero);
+   }
+
+   /**
+    * Funcion que busca si existe o no un archivo
+    * @param rutaFichero a buscar
+    * @return true or false
+    */
+   public boolean buscar(String rutaFichero) {
+      File file;
+      boolean ficheroExiste = false;
+      file = new File(rutaFichero);
+      ficheroExiste = file.exists();
+      return ficheroExiste;
+   }
+
+   /**
+    * Metodo que modifica el texto insertado
+    * 
+    * @param rutaFichero donde modificar
+    * @throws FicheroException controlado
+    */
+   public void modificar(String rutaFichero) throws FicheroException {
+      String modificacion = "Texto cambiado";
+      fichero.crear(rutaFichero, modificacion);
+   }
+
+   /**
+    * Funcion que lee un fichero
+    * 
+    * @param rutaFichero del que leer algo
+    * @throws FicheroException error controlado
+    */
+   public String leer(String rutaFichero) throws FicheroException {
       return fichero.leer(rutaFichero);
-     }
+   }
 }
--- mvcUsuarios/mvnUsuarios/src/test/java/es/iesptocruz/victor/FicheroTest.java
+++ mvcUsuarios/mvnUsuarios/src/test/java/es/iesptocruz/victor/FicheroTest.java
@@ -46,4 +46,33 @@ public class FicheroTest {
         userModelo.insertar(ruta, "Mundo");
         assertEquals("Debe sobrescribir el archivo", "Mundo", userModelo.leer(ruta));
     }
+
+    /**
+     * Test que modifica el contenido del fichero
+     * @throws FicheroException controlado
+     */
+    @Test
+    public void modificarTest() throws FicheroException{
+        userModelo.modificar(ruta);
+        assertEquals("Debe modificar el archivo", "Texto cambiado",
+        userModelo.leer(ruta));
+    }
+
+    /**
+     * Test que demuestra que busca si existe el archivo
+     */
+    @Test
+    public void buscarTest(){
+        assertEquals("Debe existir el archivo", true,
+        userModelo.buscar(ruta));
+    }
+
+    /**
+     * Test que demuestra que falla si no existe el archivo
+     */
+    @Test
+    public void noBuscarTest(){
+        assertEquals("No debe existir el archivo", false,
+        userModelo.buscar("algo.txt"));
+    }
 }
