838d7d44f31a1084a50ad5facebe2f359dd8faef
kratossore069 <victorgabrielarostegui@gmail.com>
2021-05-17 12:43:12 +0100

    Adelantos con el main

--- alumnosClase/alumnosclase/src/main/java/es/iesptocruz/victor/vista/App.java
+++ alumnosClase/alumnosclase/src/main/java/es/iesptocruz/victor/vista/App.java
@@ -1,21 +1,66 @@
 import java.util.Scanner;
 
+import es.iesptocruz.victor.api.Alumno;
+import es.iesptocruz.victor.controlador.AlumnoController;
+
 public class App {
 
-    // public static void main(String[] args) {
-    //     Scanner sc = new Scanner(System.in);
-    //     System.out.println("Introduzca su opcion para empezar con el programa: ");
-    //     int opcion = sc.nextInt();
-    //     while (opcion != 0) {
-    //         System.out.println("Pulse 1 para añadir un alumno");
-    //         System.out.println("Pulse 2 para eliminar un alumno");
-    //         System.out.println("Pulse 3 para buscar un alumno");
-    //         System.out.println("Pulse 4 para listar los alumnos");
-    //         System.out.println("Pulse 0 para salir");
-    //     }
-    // }
-    
     public static void main(String[] args) {
-        
+
+        Scanner sc = new Scanner(System.in);
+        AlumnoController controlador=new AlumnoController();
+
+        System.out.println("Introduzca su opcion para empezar con el programa: ");
+        int opcion = sc.nextInt();
+        while (opcion != 0) {
+            System.out.println("Pulse 1 para añadir un alumno");
+            System.out.println("Pulse 2 para eliminar un alumno");
+            System.out.println("Pulse 3 para buscar un alumno");
+            System.out.println("Pulse 4 para listar los alumnos");
+            System.out.println("Pulse 0 para salir");
+            int elegir=sc.nextInt();
+
+            switch(elegir){
+                case 1:
+                    System.out.println("Nombre: ");
+                    String nombre=sc.nextLine();
+                    System.out.println("Apellido: ");
+                    String apellido=sc.nextLine();
+                    System.out.println("Numero: ");
+                    int numero=sc.nextInt();
+                    Alumno alumno=new Alumno(nombre,apellido,numero);
+                    controlador.aniadirAlumno(alumno);
+                    break;
+                case 2:
+                    System.out.println("Nombre: ");
+                    String nombre=sc.nextLine();
+                    System.out.println("Apellido: ");
+                    String apellido=sc.nextLine();
+                    System.out.println("Numero: ");
+                    int numero=sc.nextInt();
+                    Alumno alumno=new Alumno(nombre,apellido,numero);
+                    controlador.eliminarAlumno(alumno);
+                    break;
+                case 3:
+                    System.out.println("Nombre: ");
+                    String nombre=sc.nextLine();
+                    System.out.println("Apellido: ");
+                    String apellido=sc.nextLine();
+                    System.out.println("Numero: ");
+                    int numero=sc.nextInt();
+                    Alumno alumno=new Alumno(nombre,apellido,numero);
+                    controlador.buscarAlumno(alumno);
+                    break;
+                case 4:
+                    controlador.listaAlumno();
+                    break;
+                case 0:
+                    System.out.println("Saliendo... ");
+                    break;
+                default:
+                    System.out.println("Solamente números del 0 al 4");
+                    break;
+            }
+        }
     }
 }
