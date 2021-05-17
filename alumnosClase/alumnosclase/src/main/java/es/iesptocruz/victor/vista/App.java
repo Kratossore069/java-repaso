3ee0520b72991a460d77a9cf63c498476a76b26d
kratossore069 <victorgabrielarostegui@gmail.com>
2021-05-17 11:24:16 +0100

    Intento de vista

---
+++ alumnosClase/alumnosclase/src/main/java/es/iesptocruz/victor/vista/App.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class App {
+
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Introduzca su opcion para empezar con el programa: ");
+    //     int opcion = sc.nextInt();
+    //     while (opcion != 0) {
+    //         System.out.println("Pulse 1 para añadir un alumno");
+    //         System.out.println("Pulse 2 para eliminar un alumno");
+    //         System.out.println("Pulse 3 para buscar un alumno");
+    //         System.out.println("Pulse 4 para listar los alumnos");
+    //         System.out.println("Pulse 0 para salir");
+    //     }
+    // }
+    
+    public static void main(String[] args) {
+        
+    }
+}
